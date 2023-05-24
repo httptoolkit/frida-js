d0ab7fa7c244fdeb244040a22bc67cc9ad6d3e80
Tim Perry <pimterry@gmail.com>
2023-05-24 17:03:24 +0200

    Add an max runtime & more initial delay for our server test fixture
    Reliable locally, but occasionally doesn't exit or fails because socket
    isn't yet open in CI.

--- test/fixtures-setup/serve/src/main.rs
+++ test/fixtures-setup/serve/src/main.rs
@@ -1,9 +1,11 @@
+use std::time::Duration;
 use std::{env, process};
 use std::sync::Arc;
 use std::net::SocketAddr;
 use std::convert::Infallible;
 use hyper::{Body, Request, Response, Server, service::{make_service_fn, service_fn}, Error as HyperError};
 use tokio::task;
+use tokio::time::sleep;
 
 // A test web server: takes a message on the command-line, then servers it back to the
 // first request, and then shut down.
@@ -15,6 +17,13 @@ pub async fn main() {
     // before the server starts running...
     let args: Vec<String> = env::args().collect();
     let message = format!("{:?}", &args[1..]);
+
+    task::spawn(async {
+        sleep(Duration::from_millis(2000)).await;
+        println!("Exiting due to timeout");
+        process::exit(2);
+    });
+
     serve_message(message.as_str()).await;
 }
 
@@ -34,6 +43,7 @@ pub async fn serve_message(message: &str) {
                 async move {
                     // Shutting down the server happily, just after this response completes
                     task::spawn(async {
+                        println!("Exiting after request");
                         process::exit(0);
                     });
 
@@ -53,7 +63,7 @@ pub async fn serve_message(message: &str) {
 
     if let Err(e) = server.await {
         // Log, then shut down the server unhappily on any errors
-        eprintln!("Server error: {}", e);
+        eprintln!("Exiting after server error: {}", e);
         process::exit(1);
     }
 }
