8e252ba6508f0fd6943caf94c0d6c59af2a37c71
Tim Perry <pimterry@gmail.com>
2024-05-16 09:02:23 +0200

    Improve test Frida server management
    Do a proper connectivity check for startup, and improve management of
    shutdown to make sure it's really really dead and eliminate any risks of
    race conditions or similar when starting/stopping repeatedly.

--- test/run-frida-server.ts
+++ test/run-frida-server.ts
@@ -1,11 +1,15 @@
 import * as path from 'path';
 import { spawn, ChildProcess } from 'child_process';
+import * as net from 'net';
+
 import { delay } from './test-util';
 
 let fridaServer: ChildProcess | undefined;
 
 export const FRIDA_SERVER_DIR = path.join(__dirname, '.frida-server');
 export const FRIDA_SERVER_BIN = path.join(FRIDA_SERVER_DIR, 'frida-server');
+
+const FRIDA_PORT = 27042;
 
 export async function startFridaServer() {
     if (fridaServer) throw new Error("Can't start Frida server when one is already running");
@@ -24,25 +28,51 @@ export async function startFridaServer() {
         fridaServer!.on('spawn', resolve);
     });
 
-    // Add a little delay just to make sure the server is definitely started & ready
-    await delay(500);
+    // Wait until the server is reachable:
+    while (true) {
+        const reachable = await new Promise<boolean>((resolve) => {
+            const socket = net.createConnection({
+                host: '127.0.0.1',
+                port: FRIDA_PORT
+            });
+            socket.on('connect', () => {
+                socket.destroy();
+                resolve(true);
+            });
+            socket.on('error', (e) => {
+                resolve(false)
+            });
+        });
+
+        if (reachable) break;
+        else await delay(100);
+    }
 
     return fridaServer;
 }
 
 export async function stopFridaServer() {
     if (!fridaServer) return;
-    if (fridaServer.exitCode !== null) return;
+    if (fridaServer.exitCode !== null) {
+        console.log(`Frida already exited with ${fridaServer.exitCode}`);
+        return;
+    }
 
-    const exitPromise = new Promise((resolve) => {
-        fridaServer!.on('exit', resolve);
+    const closePromise = new Promise((resolve) => {
+        fridaServer!.on('close', resolve);
     });
 
     fridaServer.kill();
 
-    await Promise.race([
-        exitPromise,
-        // Sometimes Frida doesn't seem to exit cleanly. Just kill it instead if need be:
-        delay(500).then(() => fridaServer?.kill('SIGKILL'))
+    const cleanExit = await Promise.race([
+        closePromise.then(() => true),
+        delay(500).then(() => false)
     ]);
+
+    // Sometimes Frida doesn't seem to exit cleanly. Just kill it instead if need be:
+    if (!cleanExit) {
+        console.log('Frida did not exit cleanly - sending SIGKILL');
+        fridaServer?.kill('SIGKILL');
+        await closePromise;
+    }
 }
