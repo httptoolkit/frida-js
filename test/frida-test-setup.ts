91030c82d36658a8e949cc7006b5af26f62015cf
Tim Perry <pimterry@gmail.com>
2024-05-16 14:48:42 +0200

    Start & stop Frida between every Node test
    This solves some issues where it can become totally unresponsive -
    ensuring that subsequent tests recover, and that it's cleaned up so that
    CI retries work successfully.
    
    Note that the browser tests still use a single Frida process throughout.

--- test/frida-test-setup.ts
+++ test/frida-test-setup.ts
@@ -3,8 +3,8 @@ import { delay, isNode } from './test-util';
 if (isNode) {
     const { startFridaServer, stopFridaServer } = require("./run-frida-server") as typeof import('./run-frida-server');
 
-    before(() => startFridaServer());
-    after(() => stopFridaServer());
+    beforeEach(() => startFridaServer());
+    afterEach(() => stopFridaServer());
 } else {
     before(async () => {
         // Wait just a moment to give Frida (started via package.json script) time to start up.
